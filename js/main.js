b12c5ef993dba04fa250d55b600c47292c784f98
Carlos <carlosjesus94@gmail.com>
2016-07-11 21:28:17 -0400

    Add who we are section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function () {
+  $(".weare").typed({
+    strings: ["Geeks.", "Engineers.", "Developers.", "Code Monkeys."],
+    showCursor: true,
+    typeSpeed: 100,
+    backDelay: 1800,
+    backSpeed: 50
+  });
+} ());
