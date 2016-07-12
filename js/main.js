cbd7e00da5fe1e7895c44e8520fd6a667df4892c
Carlos <carlosjesus94@gmail.com>
2016-07-11 22:17:36 -0400

    Fix mobile animations

--- js/main.js
+++ js/main.js
@@ -1,39 +1,13 @@
 (function () {
   $(document).ready(function () {
-    setTypedScroll(
-      ".section-whoweare", 
-      ".weare", 
-      ["Geeks.", 
-       "Engineers.", 
-       "Developers.", 
-       "Code Monkeys."]);
+    $(".weare").typed({
+      strings: ["Geeks.", "Engineers.", "Developers.", "Code Monkeys."],
+      showCursor: true,
+      typeSpeed: 100,
+      backDelay: 1800,
+      backSpeed: 50
+    });
 
     new WOW().init();
   });
-
-  function setTypedScroll(sectionToScroll, blockToAnimate, phrases) {
-    var triggered = false;
-
-    $(window).scroll(function() {    
-      if(isScrolledIntoView($(sectionToScroll)) && !triggered) {
-        $(blockToAnimate).typed({
-          strings: phrases,
-          showCursor: true,
-          typeSpeed: 100,
-          backDelay: 1800,
-          backSpeed: 50
-        });
-
-        triggered = true;
-      }    
-    });
-  }
-  
-  function isScrolledIntoView(elem) {
-      var docViewTop = $(window).scrollTop();
-      var docViewBottom = docViewTop + $(window).height();
-      var elemTop = $(elem).offset().top;
-      var elemBottom = elemTop + $(elem).height();
-      return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-  }
 } ());
