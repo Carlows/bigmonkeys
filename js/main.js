a93be530fd0f9a3d063665cd2a6573d9cc3392e6
Carlos <carlosjesus94@gmail.com>
2016-07-11 21:59:52 -0400

    Animate all the things

--- js/main.js
+++ js/main.js
@@ -1,9 +1,39 @@
 (function () {
-  $(".weare").typed({
-    strings: ["Geeks.", "Engineers.", "Developers.", "Code Monkeys."],
-    showCursor: true,
-    typeSpeed: 100,
-    backDelay: 1800,
-    backSpeed: 50
+  $(document).ready(function () {
+    setTypedScroll(
+      ".section-whoweare", 
+      ".weare", 
+      ["Geeks.", 
+       "Engineers.", 
+       "Developers.", 
+       "Code Monkeys."]);
+
+    new WOW().init();
   });
+
+  function setTypedScroll(sectionToScroll, blockToAnimate, phrases) {
+    var triggered = false;
+
+    $(window).scroll(function() {    
+      if(isScrolledIntoView($(sectionToScroll)) && !triggered) {
+        $(blockToAnimate).typed({
+          strings: phrases,
+          showCursor: true,
+          typeSpeed: 100,
+          backDelay: 1800,
+          backSpeed: 50
+        });
+
+        triggered = true;
+      }    
+    });
+  }
+  
+  function isScrolledIntoView(elem) {
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+      var elemTop = $(elem).offset().top;
+      var elemBottom = elemTop + $(elem).height();
+      return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
 } ());
